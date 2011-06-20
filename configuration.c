a9e7e0656627f3b7dc1bee50dbc1e6f936d62c13
julp <julp@redaction-developpez.com>
2011-06-20 21:57:03 +0200

    cleanups

--- configuration.c
+++ configuration.c
@@ -1,10 +1,18 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <libxml/parser.h>
 #include <libxml/xpath.h>
 
 #include "port/asprintf.c"
+
+#define NOM_RACINE "configuration"
 
 typedef struct {
     char *fichier;
@@ -14,12 +22,18 @@ typedef struct {
 } xmlConfig_t;
 
 /**
- * Libération de la mémoire pour la structure xmlConfig_t
+ * Libération des ressources de la structure xmlConfig_t
  **/
 void free_config(xmlConfig_t *conf) {
-    xmlXPathFreeContext(conf->ctxt);
-    xmlFreeDoc(conf->doc);
-    free(conf->fichier);
+    if (NULL != conf->ctxt) {
+        xmlXPathFreeContext(conf->ctxt);
+    }
+    if (NULL != conf->doc) {
+        xmlFreeDoc(conf->doc);
+    }
+    if (NULL != conf->fichier) {
+        free(conf->fichier);
+    }
     free(conf);
 }
 
@@ -27,37 +41,49 @@ void free_config(xmlConfig_t *conf) {
  * Initialisation et chargement du fichier XML en mémoire
  **/
 xmlConfig_t *load_config(const char *fichier) {
+    struct stat sts;
     xmlConfig_t *conf, null = { 0 };
 
-    if ((conf = malloc(sizeof *conf)) == NULL) {
+    if (NULL == (conf = malloc(sizeof(*conf)))) {
         return NULL;
     }
     memcpy(conf, &null, sizeof(*conf));
     // Copie du nom du fichier
-    if ((conf->fichier = strdup(fichier)) == NULL) {
+    if (NULL == (conf->fichier = strdup(fichier))) {
         free_config(conf);
         return NULL;
     }
     // Création de l'arbre DOM à partir du fichier XML
     xmlKeepBlanksDefault(0);
-    if ((conf->doc = xmlParseFile(conf->fichier)) == NULL) {
-        free_config(conf);
-        return NULL;
-    }
-    // Récupération de la racine
-    conf->racine = xmlDocGetRootElement(conf->doc);
-    if (NULL != conf->racine && 0 != xmlStrcasecmp(conf->racine->name, BAD_CAST "configuration")) {
-        free_config(conf);
-        return NULL;
+    if (-1 == stat(fichier, &sts)) {
+        if (ENOENT == errno) { // le fichier n'existe pas, on crée un nouvel arbre en mémoire
+            conf->doc = xmlNewDoc(BAD_CAST "1.0");
+            conf->racine = xmlNewNode(NULL, BAD_CAST NOM_RACINE);
+            xmlDocSetRootElement(conf->doc, conf->racine);
+        } else {
+            perror("stat");
+        }
+    } else { // le fichier existe : on le charge
+        if (NULL == (conf->doc = xmlParseFile(conf->fichier))) {
+            free_config(conf);
+            return NULL;
+        }
+        // Récupération de la racine
+        conf->racine = xmlDocGetRootElement(conf->doc);
+        if (NULL != conf->racine && 0 != xmlStrcasecmp(conf->racine->name, BAD_CAST NOM_RACINE)) {
+            free_config(conf);
+            return NULL;
+        }
     }
     // Initialisation de l'environnement XPath
     xmlXPathInit();
     // Création d'un contexte pour les requêtes XPath
     conf->ctxt = xmlXPathNewContext(conf->doc);
-    if (conf->ctxt == NULL) {
+    if (NULL == conf->ctxt) {
         free_config(conf);
         return NULL;
     }
+
     return conf;
 }
 
@@ -76,10 +102,10 @@ static xmlNodePtr _get_node_by_xpath(xmlConfig_t *conf, const char *directive) {
     }
     // Evaluation de l'expression XPath
     xpathRes = xmlXPathEvalExpression(BAD_CAST path, conf->ctxt);
-    free(path);
     if (NULL != xpathRes && XPATH_NODESET == xpathRes->type && 1 == xpathRes->nodesetval->nodeNr) {
         n = xpathRes->nodesetval->nodeTab[0];
     }
+    free(path);
     xmlXPathFreeObject(xpathRes);
 
     return n;
@@ -88,10 +114,10 @@ static xmlNodePtr _get_node_by_xpath(xmlConfig_t *conf, const char *directive) {
 /**
  * Renvoie la valeur d'une directive
  **/
-const char *get_config(xmlConfig_t *conf, const char *directive) {
+const xmlChar *get_config(xmlConfig_t *conf, const char *directive) {
     xmlNodePtr n;
 
-    if (NULL != (n =  _get_node_by_xpath(conf, directive))) {
+    if (NULL != (n = _get_node_by_xpath(conf, directive))) {
         return xmlNodeGetContent(n);
     }
 
@@ -117,7 +143,7 @@ int set_config(xmlConfig_t *conf, const char *directive, const char *valeur) {
     } else { // La directive existe : modification
         xmlNodeSetContent(n, BAD_CAST valeur);
     }
-    return (xmlSaveFormatFile(conf->fichier, conf->doc, 1) > 0 ? 1 : 0);
+    return !!xmlSaveFormatFile(conf->fichier, conf->doc, 1);
 }
 
 
@@ -125,16 +151,18 @@ int set_config(xmlConfig_t *conf, const char *directive, const char *valeur) {
  * Affichage de la valeur d'une directive
  **/
 void print_config(xmlConfig_t *conf, const char *directive) {
-    const char *val = get_config(conf, directive);
+    const xmlChar *val = get_config(conf, directive);
 
-    printf("Valeur de '%s' : %s\n", directive, val != NULL ? val : "(directive inconnue)");
+    printf("Valeur de '%s' : %s\n", directive, NULL != val ? (char *) val : "(directive inconnue)");
 }
 
 int main() {
     xmlConfig_t *conf;
 
     // Chargement du fichier et initialisation
-    conf = load_config("configuration.xml");
+    if (NULL == (conf = load_config("configuration.xml"))) {
+        return EXIT_FAILURE;
+    }
 
     // Récupération de la valeur des directives
     print_config(conf, "directive_inexistante");
